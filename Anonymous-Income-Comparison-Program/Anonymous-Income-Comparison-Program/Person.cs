6eabc089a7afe9823bb72e0de3d623af01d1d502
Christopher Goodburn <c.lg11@hotmail.com>
2018-07-16 19:17:38 -0700

    Added new project

---
+++ Anonymous-Income-Comparison-Program/Anonymous-Income-Comparison-Program/Person.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Anonymous_Income_Comparison_Program
+{
+    class Person
+    {
+        private double hourlyRate;
+        private double hoursWorked;
+
+        public void SetHourlyRate(string hourlyRate)
+        {
+            this.hourlyRate = Convert.ToDouble(hourlyRate);
+        }
+
+        public void SetHoursWorked(string hoursWorked)
+        {
+            this.hoursWorked = Convert.ToDouble(hoursWorked);
+        }
+        
+        public double GetHourlyRate()
+        {
+            return this.hourlyRate;
+        }
+
+        public double GetHoursWorked()
+        {
+            return this.hoursWorked;
+        }
+
+        public int GetAnnualSalary()
+        {
+            double moneyPerWeek = this.hourlyRate * this.hoursWorked;
+            //A year has 52 weeks in it so moneyPerWeek times 52 equals annual salary
+            int annualSalary = (int)moneyPerWeek * 52;
+            return annualSalary;
+        }
+    }
+}
---
+++ Anonymous-Income-Comparison-Program/Anonymous-Income-Comparison-Program/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Anonymous_Income_Comparison_Program
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person person1 = new Person();
+            Person person2 = new Person();
+
+            Console.WriteLine("Anonymous Income Comparison Program");
+
+            Console.WriteLine("Person 1: ");
+            Console.WriteLine("Hourly Rate?");
+            person1.SetHourlyRate(Console.ReadLine());
+            Console.WriteLine("Hours worked per week?");
+            person1.SetHoursWorked(Console.ReadLine());
+
+            Console.WriteLine("Person 2: ");
+            Console.WriteLine("Hourly Rate?");
+            person2.SetHourlyRate(Console.ReadLine());
+            Console.WriteLine("Hours worked per week?");
+            person2.SetHoursWorked(Console.ReadLine());
+
+            Console.WriteLine("Annual salary of Person 1: ");
+            Console.WriteLine(person1.GetAnnualSalary());
+            Console.WriteLine("Annual salary of Person 2: ");
+            Console.WriteLine(person2.GetAnnualSalary());
+
+            Console.WriteLine("Does Person 1 make more money than Person 2?");
+            Console.WriteLine(person1.GetAnnualSalary() > person2.GetAnnualSalary());
+        }
+    }
+}
