c96ec3fabf4d86463164e98cad566f01039aa722
Christopher Goodburn <c.lg11@hotmail.com>
2018-07-17 21:29:12 -0700

    Fixed a project

--- C#-Class/While-Statement-Example/While-Statement-Example/Program.cs
+++ C#-Class/While-Statement-Example/While-Statement-Example/Program.cs
@@ -10,6 +10,50 @@ namespace While_Statement_Example
     {
         static void Main(string[] args)
         {
+
+            //1.
+            Console.WriteLine("Guess a letter. If you get it right you win but if not you have to keep guessing!");
+            char guess = Convert.ToChar(Console.ReadLine());
+            bool keepGoing = true;
+
+            while (keepGoing)
+            {
+                switch (guess)
+                {
+                    case 'a':
+                        Console.WriteLine("Sorry 'a' is the wrong guess");
+                        Console.WriteLine("Guess another letter!");
+                        guess = Convert.ToChar(Console.ReadLine());
+                        break;
+                    case 'g':
+                        Console.WriteLine("Sorry 'g' is the wrong guess");
+                        Console.WriteLine("Guess another letter!");
+                        guess = Convert.ToChar(Console.ReadLine());
+                        break;
+                    case 'z':
+                        Console.WriteLine("Sorry 'z' is the wrong guess");
+                        Console.WriteLine("Guess another letter!");
+                        guess = Convert.ToChar(Console.ReadLine());
+                        break;
+                    case 't':
+                        Console.WriteLine("Sorry 't' is the wrong guess");
+                        Console.WriteLine("Guess another letter!");
+                        guess = Convert.ToChar(Console.ReadLine());
+                        break;
+                    case 'i':
+                        Console.WriteLine("You got it correct! 'i' is the right letter!");
+                        keepGoing = false;
+                        break;
+                    default:
+                        Console.WriteLine("Sorry, wrong guess");
+                        Console.WriteLine("Guess another letter!");
+                        guess = Convert.ToChar(Console.ReadLine());
+                        break;
+
+                }
+            }
+            /*
+            // 2.
             bool keepGoing = true;
             Console.WriteLine("Guess a letter. If you get it right you win but if not you have to keep guessing!");
             char guess = Convert.ToChar(Console.ReadLine());
@@ -50,6 +94,7 @@ namespace While_Statement_Example
                 }
             }
             while (keepGoing);
+        }*/
         }
     }
 }
