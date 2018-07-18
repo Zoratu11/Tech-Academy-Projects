f55c92086e6889797dcc707bef85fdfcb8f3330d
Christopher Goodburn <c.lg11@hotmail.com>
2018-07-18 12:53:55 -0700

    Added a new project

---
+++ C#-Class/Method-Overloading-Exercise/Method-Overloading-Exercise/MyMath.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Method_Overloading_Exercise
+{
+    class MyMath
+    {
+        public int DoMath(int number)
+        {
+            return number + 100;
+        }
+
+        public int DoMath(decimal number)
+        {
+            decimal number2 = 12.5m;
+            number = number * number2;
+            return (int)number;
+        }
+
+        public int DoMath(string number)
+        {
+            int number1 = 0;
+            try
+            {
+                number1 = Convert.ToInt32(number);
+            }
+            catch(FormatException ex)
+            {
+                Console.WriteLine("You were supposed to enter a number!");
+            }
+
+            return number1 - 10;
+
+        }
+    }
+}
---
+++ C#-Class/Method-Overloading-Exercise/Method-Overloading-Exercise/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Method_Overloading_Exercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyMath math1 = new MyMath();
+            MyMath math2 = new MyMath();
+            MyMath math3 = new MyMath();
+
+            int number1 = 12;
+            decimal number2 = 4.32m;
+            string number3 = "20";
+
+            Console.WriteLine("After doing some math on " + number1 + " the result is: " + math1.DoMath(number1));
+            Console.WriteLine("After doing some math on " + number2 + " the result is: " + math1.DoMath(number2));
+            Console.WriteLine("After doing some math on " + number3 + " the result is: " + math1.DoMath(number3));
+
+            Console.ReadLine();
+        }
+    }
+}
