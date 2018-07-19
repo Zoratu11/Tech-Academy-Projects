01325b08d196106d32dceb5186cacd826bc4f74c
Christopher Goodburn <c.lg11@hotmail.com>
2018-07-18 20:51:59 -0700

    Added a new project

---
+++ C#-Class/Named-Parameter-Exercise/Named-Parameter-Exercise/MyMath.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Named_Parameter_Exercise
+{
+    class MyMath
+    {
+        public void DoMath(int num1, int num2)
+        {
+            num1 = num1 + 37;
+            Console.WriteLine("Here is a number: " + num2);
+        }
+    }
+}
---
+++ C#-Class/Named-Parameter-Exercise/Named-Parameter-Exercise/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Named_Parameter_Exercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyMath math = new MyMath();
+            math.DoMath(12, 43);
+            math.DoMath(num1: 5, num2: 10);
+
+            Console.ReadLine();
+        }
+    }
+}
