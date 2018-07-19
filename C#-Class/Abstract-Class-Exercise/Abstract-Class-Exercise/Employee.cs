50e7e4a59f8024422500b705b5b3da17395a630b
Christopher Goodburn <c.lg11@hotmail.com>
2018-07-19 15:49:37 -0700

    Added Abstract-Class-Exercise

---
+++ C#-Class/Abstract-Class-Exercise/Abstract-Class-Exercise/Employee.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Abstract_Class_Exercise
+{
+    public class Employee : Person
+    {
+        public override void SayName()
+        {
+            Console.WriteLine("The name is: " + FirstName + " " + LastName);
+        }
+    }
+}
