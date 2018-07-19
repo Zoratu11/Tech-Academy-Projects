8df6d02c631f0c352518ff6f8a70e92a8cf7a102
Christopher Goodburn <c.lg11@hotmail.com>
2018-07-18 18:02:22 -0700

    Added a new project

---
+++ C#-Class/Optional-Parameter-Exersice/Optional-Parameter-Exersice/MyMath.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Optional_Parameter_Exersice
+{
+    class MyMath
+    {
+        public int doMath(int num1, int num2 = 9)
+        {
+            return num1 * num2;
+        }
+    }
+}
---
+++ C#-Class/Optional-Parameter-Exersice/Optional-Parameter-Exersice/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Optional_Parameter_Exersice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyMath math = new MyMath();
+
+            Console.WriteLine("Enter 1 or 2 numbers, if you " +
+                "enter 2 numbers enter them one by one.\n" +
+                "Leave the second line blank if you don't " +
+                "want to enter another number: ");
+            int num1 = Convert.ToInt32(Console.ReadLine());
+            string str = Console.ReadLine();
+            int num2;
+
+            if(str == "")
+            {
+                int num3 = math.doMath(num1);
+                Console.WriteLine("Here is your number after doing some math: " + num3);
+            } else
+            {
+                num2 = Convert.ToInt32(str);
+                int num3 = math.doMath(num1, num2);
+                Console.WriteLine("Here is your number after doing some math: " + num3);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
