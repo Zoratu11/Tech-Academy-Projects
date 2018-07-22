a1b17bb44c170ce13b4f16bec129582acffc066e
Christopher Goodburn <c.lg11@hotmail.com>
2018-07-22 09:43:34 -0700

    Added Input-Output-Exercise

---
+++ C#-Class/Input-Output-Exercise/Input-Output-Exercise/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Input_Output_Exercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter a number!");
+            string text = Console.ReadLine();
+            using (StreamWriter file = new StreamWriter(@"C:\Users\Zoratu\Desktop\Logs\log1.txt", true))
+            {
+                file.WriteLine(text);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
